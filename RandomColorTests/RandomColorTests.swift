ffd75d4f274f341516745a2330b7d158a8947bb2
mgKhantt <khantphonenaing156@gamil.com>
2024-10-16 18:46:04 +0630

    Initial Commit

---
+++ RandomColorTests/RandomColorTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomColorTests.swift
+//  RandomColorTests
+//
+//  Created by Khant Phone Naing  on 16/10/2024.
+//
+
+import Testing
+@testable import RandomColor
+
+struct RandomColorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
